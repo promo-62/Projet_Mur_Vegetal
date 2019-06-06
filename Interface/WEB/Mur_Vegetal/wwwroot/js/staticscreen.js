cc44c1bb09d3637dc57a491aab7b4bda86bbfe9d
Louis VIVIER <22093201+louisvivier@users.noreply.github.com>
2019-06-06 21:17:35 +0200

    [WEB] New design
    Nouveau design.
    Nouvelle page admin (formulaire) (manque css).
    Toujours pas de version présentable pour l'écran à l'accueil.
    Nouvelle police.

--- Interface/WEB/Mur_Vegetal/wwwroot/js/staticscreen.js
+++ Interface/WEB/Mur_Vegetal/wwwroot/js/staticscreen.js
@@ -1,38 +1,61 @@
+var elements = [];
+var displayTimer = 1000;
+var a = document.getElementById("a");
+var a1 = a.getElementsByClassName("wall-block");
+var b = document.getElementById("b");
+var b1 = b.getElementsByClassName("news-block");
+var c = document.getElementById("c");
+var d = document.getElementById("d");
+var d1 = document.getElementsByClassName("medias-block");
+var e = document.getElementById("e");
+a.style.display = "block";
+b.style.display = "block";
+c.style.display = "none";
+d.style.display = "block";
+e.style.display = "none";
 
 
-function myFunction() {
-    
-    var a = document.getElementById("a");
-    var b = document.getElementById("b");
-    var c = document.getElementById("c");
-    a.style.display = "none";
-    b.style.display = "none";
-    c.style.display = "none";
-    
-    if (a.style.display === "none") {
-      a.style.display = "block";
-      b.style.display = "none";
-      c.style.display = "none";
-    } else if(b === "none"){
-      a.style.display = "none";
-      c.style.display = "none";
-      b.style.display = "block";
+elements.push(c, e);
+fillElments();  
+async function defile() {
+
+  for (let i = 0; i < elements.length; i++) {
+    if (i - 1 < 0) {
+      elements[elements.length - 1].style.display = 'none';
+    } else {
+      elements[i - 1].style.display = 'none';
     }
-    else if (c === "none") {
-      a.style.display = "none";
-      b.style.display = "none";
-      c.style.display = "block";
-    }
-   
-  } 
-  
-      
-    setInterval(() => {
-            myFunction();
-        }, 2000);
-        
-      
-  
+    elements[i].style.display = 'flex';
+    await wait1Second();
 
-   
+  }
+ 
+}
+
+function wait1Second(x) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(x);
+    }, displayTimer);
+  });
+}
+
+setInterval(() => {
+  defile();
+}, elements.length*displayTimer);
+
+function fillElments(){
+    for (let index = 0; index < b1.length; index++) {
+      b1[index].style.display = "none";
+      elements.push(b1[index]);
+    }
+    for (let index = 0; index < a1.length; index++) {
+      a1[index].style.display = "none";
+      elements.push(a1[index]);
+    }
+    for (let index = 0; index < d1.length; index++) {
+      d1[index].style.display = "none";
+      elements.push(d1[index]);
+    }
+}
  
