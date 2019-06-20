6b6857ad53577f076b454566a48c7fb4e301992d
Louis VIVIER <22093201+louisvivier@users.noreply.github.com>
2019-06-20 16:04:11 +0200

    [WEB]

--- Interface/WEB/Mur_Vegetal/Startup.cs
+++ Interface/WEB/Mur_Vegetal/Startup.cs
@@ -48,7 +48,6 @@ namespace Mur_Vegetal
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
             app.UsePathBase("/web");
 
             app.Use((context, next) =>
@@ -57,7 +56,6 @@ namespace Mur_Vegetal
                 return next();
             });
             app.UseStaticFiles(); // DON'T FORGET THE LEADING SLASH!
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
