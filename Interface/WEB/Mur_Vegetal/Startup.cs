65be0bca412bd7d478597b77ed507d59d4a80fc5
Louis VIVIER <22093201+louisvivier@users.noreply.github.com>
2019-06-20 14:47:17 +0200

    [WEB] Fix links
    TRY AGAIN

--- Interface/WEB/Mur_Vegetal/Startup.cs
+++ Interface/WEB/Mur_Vegetal/Startup.cs
@@ -48,7 +48,14 @@ namespace Mur_Vegetal
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UsePathBase("/web"); // DON'T FORGET THE LEADING SLASH!
+            app.UsePathBase("/web");
+
+            app.Use((context, next) =>
+            {
+                context.Request.PathBase = "/web";
+                return next();
+            });
+            app.UseStaticFiles(); // DON'T FORGET THE LEADING SLASH!
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
