a6ae92e85541560842f8f0932421fff45a09b2a3
Etienne Schel <zxzx00@hotmail.fr>
2019-06-21 11:54:24 +0200

    Final bug fix startup.cs

--- Serveur/BDD/WebAPI/Startup.cs
+++ Serveur/BDD/WebAPI/Startup.cs
@@ -22,7 +22,7 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<WebAPIService>();
+            services.AddScoped<CapteurService>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         }
