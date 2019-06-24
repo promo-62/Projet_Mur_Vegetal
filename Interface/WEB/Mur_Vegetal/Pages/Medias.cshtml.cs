b0db930fe43f22acb6c45589914de7120b814dbd
Louis VIVIER <22093201+louisvivier@users.noreply.github.com>
2019-06-24 17:01:44 +0200

    [WEB] Fix CSS

--- Interface/WEB/Mur_Vegetal/Pages/Medias.cshtml.cs
+++ Interface/WEB/Mur_Vegetal/Pages/Medias.cshtml.cs
@@ -4,29 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.IO;
-using System.Net;
 
 namespace Mur_Vegetal.Pages
 {
     public class MediasModel : PageModel
     {
-        public string Answer { get; private set; }
-        public void OnGet(){
-            Answer = Get("https://my-json-server.typicode.com/typicode/demo/posts");
-        }
-
-        public string Get(string uri)
+        public void OnGet()
         {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
-            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
 
-            using(HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-            using(Stream stream = response.GetResponseStream())
-            using(StreamReader reader = new StreamReader(stream))
-            {
-                return reader.ReadToEnd();
-            }
         }
     }
 }
