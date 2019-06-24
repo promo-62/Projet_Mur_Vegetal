c160d03d8a8a3992efdd9031b18d70f21f8ae119
AV <alexandre.verept@isen.yncrea.fr>
2019-06-24 11:14:21 +0200

    Https -> http

--- Serveur/BDD/WebAPI/Program.cs
+++ Serveur/BDD/WebAPI/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -20,6 +20,6 @@ namespace WebAPI
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseUrls("https://*:4001");
+                .UseUrls("http://*:4001");
     }
 }
