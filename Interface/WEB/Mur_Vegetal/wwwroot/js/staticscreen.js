1dcaa04b2c1d36ca11b04700404ff842b0517960
Desprez Thibault <thibault.desprez1@gmail.com>
2019-06-24 18:09:43 +0200

    WEB to Interface (#55)
    * [WEB] Fix CSS (#52)
    
    * [WEB] Links /web/
    
    * [WEB] Fix links (again)
    
    * [WEB] Fix links
    
    TRY AGAIN
    
    * [WEB]
    
    * [WEB] Maybe fixed links ¯\_(ツ)_/¯
    
     ¯\_(ツ)_/¯
     ¯\_(ツ)_/¯
     ¯\_(ツ)_/¯
     ¯\_(ツ)_/¯
     ¯\_(ツ)_/¯
     ¯\_(ツ)_/¯
    
    * Avec le /web c'est mieux
    
    * add gitignore .DS_Store
    
    * Removed DS_Store
    
    dedi à Loïc
    
    * [WEB] Links on staticscreen
    
    * [WEB] Fix CSS for mobile
    
    * [WEB] Put back charts credits
    
    * [WEB] Fix scrollbar mobile
    
    * [WEB] /web
    
    * Revert "Merge branch 'Interface' into Interface_WEB"
    
    This reverts commit ffb59ee6ef99a6e0e5b9c3425ef01559a7ae4985, reversing
    changes made to 5383f560168ab2cd7bb016a661d1eb7dd80a3720.
    
    * Revert
    
    * Oups
    
    * [WEB] Css countdown page
    
    * [WEB] Css wallframe SScreen
    
    * [WEB] StaticScreen + Mobile
    
    -New rolling frame function
    -New rolling slideshow function
    -Style fixed for wall frame
    -New effect for rolling frame
    -New effect for rolling slideshow
    
    -Fix scrollbar on mobiles
    -Filling forms will not zoom in on iPhones/iPad
    
    -New css for countdownpage
    
    * [WEB] /web
    
    oups
    
    * Revert "[WEB] Fix scrollbar mobile (#37)"
    
    This reverts commit 17bcc90ab59d7cb48d4b4d981728194f0aef4edf.
    
    * clean
    
    * [WEB] Staticscreen + Mobile
    
    -New rolling frame function
    -New rolling slideshow function
    -Style fixed for wall frame
    -New effect for rolling frame
    -New effect for rolling slideshow
    
    -Fix scrollbar on mobiles
    -Filling forms will not zoom in on iPhones/iPad
    
    -New css for countdownpage
    
    -News CSS for all staticscreen
    
    * [WEB] /web
    
    * [WEB] Staticscreen + Usersite
    
    * /web
    
    * Update Medias.cshtml
    
    * [WEB] Fix CSS
    
    * [WEB] (#54)
    
    * [WEB] Fix CSS (#52) (#53)
    
    * [WEB] Links /web/
    
    * [WEB] Fix links (again)
    
    * [WEB] Fix links
    
    TRY AGAIN
    
    * [WEB]
    
    * [WEB] Maybe fixed links ¯\_(ツ)_/¯
    
     ¯\_(ツ)_/¯
     ¯\_(ツ)_/¯
     ¯\_(ツ)_/¯
     ¯\_(ツ)_/¯
     ¯\_(ツ)_/¯
     ¯\_(ツ)_/¯
    
    * Avec le /web c'est mieux
    
    * add gitignore .DS_Store
    
    * Removed DS_Store
    
    dedi à Loïc
    
    * [WEB] Links on staticscreen
    
    * [WEB] Fix CSS for mobile
    
    * [WEB] Put back charts credits
    
    * [WEB] Fix scrollbar mobile
    
    * [WEB] /web
    
    * Revert "Merge branch 'Interface' into Interface_WEB"
    
    This reverts commit ffb59ee6ef99a6e0e5b9c3425ef01559a7ae4985, reversing
    changes made to 5383f560168ab2cd7bb016a661d1eb7dd80a3720.
    
    * Revert
    
    * Oups
    
    * [WEB] Css countdown page
    
    * [WEB] Css wallframe SScreen
    
    * [WEB] StaticScreen + Mobile
    
    -New rolling frame function
    -New rolling slideshow function
    -Style fixed for wall frame
    -New effect for rolling frame
    -New effect for rolling slideshow
    
    -Fix scrollbar on mobiles
    -Filling forms will not zoom in on iPhones/iPad
    
    -New css for countdownpage
    
    * [WEB] /web
    
    oups
    
    * Revert "[WEB] Fix scrollbar mobile (#37)"
    
    This reverts commit 17bcc90ab59d7cb48d4b4d981728194f0aef4edf.
    
    * clean
    
    * [WEB] Staticscreen + Mobile
    
    -New rolling frame function
    -New rolling slideshow function
    -Style fixed for wall frame
    -New effect for rolling frame
    -New effect for rolling slideshow
    
    -Fix scrollbar on mobiles
    -Filling forms will not zoom in on iPhones/iPad
    
    -New css for countdownpage
    
    -News CSS for all staticscreen
    
    * [WEB] /web
    
    * [WEB] Staticscreen + Usersite
    
    * /web
    
    * Update Medias.cshtml
    
    * [WEB] Fix CSS
    
    * [WEB] Clean

--- Interface/WEB/Mur_Vegetal/wwwroot/js/staticscreen.js
+++ Interface/WEB/Mur_Vegetal/wwwroot/js/staticscreen.js
@@ -1,5 +1,5 @@
-var elements= [] ; 
-var timers = [] ; 
+var elements = [];
+var timers = [];
 var displayTimer = 5000;
 var timerCountdown = 5000;
 var timerWall = 5000;
@@ -18,8 +18,8 @@ medias.style.display = "block";
 insta.style.display = "none";
 
 
-modifyTime(1,12000,1);
-fillElments();  
+modifyTime(1, 12000, 200000);
+fillElments();
 defile();
 launchIntervail();
 
@@ -83,59 +83,4 @@ function modifyTime(timerCountdown, timerInsta, timerWall) {
     this.timerCountdown = (timerCountdown === 1) ? displayTimer : timerCountdown;
     this.timerInsta = (timerInsta === 1) ? displayTimer : timerInsta;
     this.timerWall = (timerWall === 1) ? displayTimer : timerWall;
-}
-  if (elements.length === timers.length ) {
-    for (let i = 0; i < elements.length; i++) {
-      if (i - 1 < 0) {
-        elements[elements.length - 1].style.display = 'none';
-      } else {
-        elements[i - 1].style.display = 'none';
-      }
-      elements[i].style.display = 'flex';
-      animate(elements[i]);
-      await wait1Second(timers[i]);
-      }
-}
-
-function wait1Second(displayTimer) {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve();
-        }, displayTimer);
-    });
-}
-
-function launchIntervail() {
-    setInterval(() => {
-        defile();
-    }, timers.reduce((a, b) => a + b, 0));
-
-}
-
-function fillElments() {
-    elements.push(wall, countdown, insta);
-    timers.push(timerWall, timerCountdown, timerInsta);
-    for (let index = 0; index < tabsNews.length; index++) {
-        tabsNews[index].style.display = "none";
-        elements.push(tabsNews[index]);
-        timers.push(displayTimer);
-    }
-
-    for (let index = 0; index < tabMedias.length; index++) {
-        tabMedias[index].style.display = "none";
-        elements.push(tabMedias[index]);
-        timers.push(displayTimer);
-    }
-}
-
-function animate(element) {
-    transition.begin(element, ["opacity 0 1 1s", "transform translateX(-200px) translateX(0px) 1s ease-in-out"]);
-}
-
-function modifyTime(timerCountdown, timerInsta, timerWall) {
-    /* si 1 c'est le temps / défaut   donc si vous voulez modifier que wall / exp 
-    on met modifyTime(1,1, x )   */
-    this.timerCountdown = (timerCountdown === 1) ? displayTimer : timerCountdown ; 
-    this.timerInsta = (timerInsta === 1) ? displayTimer : timerInsta ; 
-    this.timerWall = (timerWall === 1) ? displayTimer : timerWall ; 
 }
