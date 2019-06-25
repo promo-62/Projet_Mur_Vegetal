69ab1a07bef3533820023fbf4749b8b8ed730293
AV <alexandre.verept@isen.yncrea.fr>
2019-06-25 11:43:47 +0200

    MAJ bigdata
    -Ajout de la gestion de la pompe
    -Deplacer bigdata dans serveur
    -MAJ sensorchecking
    -Ajout Statement checking
    -Ajout hub

---
+++ Serveur/BigData/Models/Countdowns.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Countdowns : ICollectionModel
+    {
+        [BsonElement("Text")]
+        public string Text { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        [BsonElement("EndingDateEvent")]
+        public long EndingDateEvent { get; set; }
+
+        [BsonElement("BeginningDateEvent")]
+        public long BeginningDateEvent { get; set; }
+
+        [BsonElement("EndingDateCountdown")]
+        public long EndingDateCountdown { get; set; }
+
+        [BsonElement("Posititon")]
+        public int Position { get; set; }
+
+        [BsonElement("Image")]
+        public string Image { get; set; }
+    }
+}
