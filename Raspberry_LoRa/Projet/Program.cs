76e723c781467ef0627a54c17eeae8891d4e6e3f
Loïc Decoster <36207909+ldecoster@users.noreply.github.com>
2019-06-26 10:35:46 +0200

    Raspberry lo ra (#66)
    * Rajout du code Protocol Lora
    
    * Modification du Config.json
    
    * Protocol (#2)
    
    * Rajout du code Protocol Lora
    
    * Modification du Config.json
    
    * rajout du code gestion messages MQTT (#3)
    
    * Rajout des liaisons entre MQTT et Protocol
    
    * AJout des id de retours
    
    * AJout des connections MQTT
    
    * Ajout des comment
    
    * Protocol (#4)
    
    * Rajout du code Protocol Lora
    
    * Modification du Config.json
    
    * Rajout des liaisons entre MQTT et Protocol
    
    * AJout des id de retours
    
    * AJout des connections MQTT
    
    * Ajout des comment
    
    * Ajout des exceptions
    
    * Ajout des wget
    
    * Rajout des code d'erreurs
    
    * Modif MQTT
    
    * Politique de la config unique
    
    * Ajout de l'ID (#9)
    
    * Raspberry mqtt (#10)
    
    * rajout du code gestion messages MQTT
    
    * rajout du code gestion messages MQTT
    
    * Ajout readme et bon code MQTT
    
    * Suppression d'un vieux dossier utilisant MQTT
    
    Reste à gérer les liens avec Antonin
    
    * Merge Protocol into Raspberry_LoRa (#18)
    
    * Ajout de l'ID
    
    * Modularisation totale du protocol
    
    * Protocol V2
    
    * Tuning MQTT
    
    * Modification des erreurs
    
    * Rajout de 0x00
    
    * Merge Protocol into Raspberry_LoRa (#24)
    
    * Ajout de l'ID
    
    * Modularisation totale du protocol
    
    * Protocol V2
    
    * Tuning MQTT
    
    * Modification des erreurs
    
    * Rajout de 0x00
    
    * Readme MQTT_client + Protocol_Client
    
    * Intergrity
    
    * Merge Protocol into Raspberry_LoRa (#40)
    
    * Ajout de l'ID
    
    * Modularisation totale du protocol
    
    * Protocol V2
    
    * Tuning MQTT
    
    * Modification des erreurs
    
    * Rajout de 0x00
    
    * Readme MQTT_client + Protocol_Client
    
    * Intergrity
    
    * Readme links
    
    * Modif Readme
    
    * Modif 2 Readme
    
    * Integrity with LoRa
    
    * Reset
    
    * Merge Protocol into Raspberry_LoRa (#59)
    
    * Ajout de l'ID
    
    * Modularisation totale du protocol
    
    * Protocol V2
    
    * Tuning MQTT
    
    * Modification des erreurs
    
    * Rajout de 0x00
    
    * Readme MQTT_client + Protocol_Client
    
    * Intergrity
    
    * Readme links
    
    * Modif Readme
    
    * Modif 2 Readme
    
    * Integrity with LoRa
    
    * Reset
    
    * syst
    
    * tunning for raspberry
    
    * merge protocol into raspberry (#62)
    
    * Ajout de l'ID
    
    * Modularisation totale du protocol
    
    * Protocol V2
    
    * Tuning MQTT
    
    * Modification des erreurs
    
    * Rajout de 0x00
    
    * Readme MQTT_client + Protocol_Client
    
    * Intergrity
    
    * Readme links
    
    * Modif Readme
    
    * Modif 2 Readme
    
    * Integrity with LoRa
    
    * Reset
    
    * syst
    
    * tunning for raspberry
    
    * Delete useless README
    
    * Merge Protocol into Raspberry_LoRa (#64)
    
    * Ajout de l'ID
    
    * Modularisation totale du protocol
    
    * Protocol V2
    
    * Tuning MQTT
    
    * Modification des erreurs
    
    * Rajout de 0x00
    
    * Readme MQTT_client + Protocol_Client
    
    * Intergrity
    
    * Readme links
    
    * Modif Readme
    
    * Modif 2 Readme
    
    * Integrity with LoRa
    
    * Reset
    
    * syst
    
    * tunning for raspberry
    
    * Delete useless README
    
    * Data size and readme

---
+++ Raspberry_LoRa/Projet/Program.cs
@@ -0,0 +1,142 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Projet
+{
+    class Program
+    {
+        //Chemin vers le fichier config.json (Formats des trames)
+        private static string filepath_1 = "Config.json";
+
+        private static string Config;
+        public static void Main(string[] args){
+            Test();
+            while(true){
+
+            }
+        }
+
+        static async Task Test(){
+            //Mise en place de la comunication entre C++ et C#
+            IPAddress ip = Dns.GetHostEntry("localhost").AddressList[1];
+            TcpListener server = new TcpListener(ip, 15200);
+            TcpClient client = default(TcpClient);
+
+            //Lancement de la comunication entre les 2 programmes (LoRA et Protocol)
+            try{
+                server.Start();
+                var client2 = new WebClient();
+                Config = client2.DownloadString(filepath_1);
+                Console.WriteLine("ADDRESS: "+ip);
+                Console.WriteLine("SERVER: STARTED");
+                Console.WriteLine("");
+            }catch(Exception e){
+                Console.WriteLine(e.ToString());
+                System.Environment.Exit(1);
+            }
+
+            Thread Update = new Thread(UpdateConfig);
+            Update.Start();
+
+            string test;
+            client = server.AcceptTcpClient();
+            NetworkStream stream = client.GetStream();
+            //stream.ReadTimeout = 3600000;
+            stream.WriteTimeout = 10000;
+            //Boucle infini
+            while(true){
+                
+                //Attend un message du LoRA
+                Console.WriteLine("WAITING FOR MESSAGE");
+                Console.WriteLine("");
+                byte[] buffer = new byte[100];
+                try{
+                    stream.Read(buffer, 0, buffer.Length);
+                }catch(Exception e){
+                    Console.WriteLine("CONNECTION LOST WITH LORA");
+                    client = server.AcceptTcpClient();
+                    stream = client.GetStream();
+                    stream.Read(buffer, 0, buffer.Length);
+                }
+                //Convertion du message recu en BYTE[] (Raw Data)
+                test = BitConverter.ToString(buffer);
+                Console.WriteLine("MESSAGE RECEIVED: "+test);
+                Console.WriteLine("");
+
+                //Envoi du message dans la section traitement du Protocol.cs et attente d'une reponse du serveur
+                //Cette section va verifier puis convertir le message en JSON
+                string response = await Protocol.DataToJson(buffer, Config);
+
+                //Verification si le message etait bien formate
+                if( !(response.Equals("")) ){
+
+                    //Convertion du JSON en BYTE[]
+                    byte[] Raw_Data = Protocol.JsonToData(response, Config);
+                    byte[] Data_To_Send = new Byte[Raw_Data.Length+1];
+                    Data_To_Send[0] = 0x00;
+                    for(int g = 0; g < Raw_Data.Length; g++){
+                        Data_To_Send[g+1] = Raw_Data[g];
+                    }
+
+                    //Envoi de la reponse au LoRA
+                    try{
+                        stream.Write(Data_To_Send, 0, Data_To_Send.Length);
+                    }catch(Exception e){
+                        Console.WriteLine("CONNECTION LOST WITH LORA");
+                        client = server.AcceptTcpClient();
+                        stream = client.GetStream();
+                    }
+                    test = BitConverter.ToString(Data_To_Send);
+                    Console.WriteLine("MESSAGE SEND: "+test);
+                    Console.WriteLine("");
+                }else{
+                    //Envoi des erreurs
+                    byte[] Errors = Protocol.ErrorsFound();
+                    try{
+                        stream.Write(Errors, 0, Errors.Length);
+                    }catch(Exception e){
+                        Console.WriteLine("CONNECTION LOST WITH LORA");
+                        client = server.AcceptTcpClient();
+                        stream = client.GetStream();
+                    }
+                    test = BitConverter.ToString(Errors);
+                    if(Errors[0].Equals(0x01)){
+                        Console.WriteLine("MESSAGE SEND: NO RESPONSE FROM DATABASE");
+                        Console.WriteLine("");
+                    }else if(Errors[0].Equals(0x02)){
+                        Console.WriteLine("MESSAGE SEND: INVALID FORMAT");
+                        Console.WriteLine("");
+                    }else if(Errors[0].Equals(0x03)){
+                        Console.WriteLine("MESSAGE SEND: INVALID PAYLOAD SIZE");
+                        Console.WriteLine("");
+                    }
+                    Console.WriteLine("ERROR SEND: "+test);
+                    Console.WriteLine("");
+                }
+            }
+        }
+
+        //Mise a jour des fichier json de config
+        private static void UpdateConfig(){
+            while(true){
+                //A remplacer par un wget
+                try{
+                    var client = new WebClient();
+                    Config = client.DownloadString(filepath_1);
+                }catch(Exception e){
+                    Console.WriteLine(e.StackTrace);
+                    System.Environment.Exit(1);
+                }
+                Thread.Sleep(600);
+            }
+        }
+
+    }
+}
