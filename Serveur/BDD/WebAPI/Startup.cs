f9a2c34906d70e132dc7c076441ea46621d1ab5b
Etienne Schel <zxzx00@hotmail.fr>
2019-06-27 18:28:29 +0200

    Update WebAPI v6.4.1 - Fix CORS

--- Serveur/BDD/WebAPI/Startup.cs
+++ Serveur/BDD/WebAPI/Startup.cs
@@ -49,14 +49,15 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.UseHsts();
+            /*if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
             else
             {
                 app.UseHsts();
-            };
+            };*/
 
             var client = new MongoClient( (string) LoadJson().ConnectionStrings.MurVegetalDb);
             var database = client.GetDatabase("MurVegetalDb");
